a4d808ff5d3fa68607f873abc0e5e2998fdf46a5
andayduman <44687213+andayduman@users.noreply.github.com>
2021-11-07 14:16:12 -0500

    getting code ready for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 //const connectDB = require("./config/db");
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 
 const app = express();
 
@@ -30,13 +31,23 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Home route
-app.get("/", (req, res) => res.json({ msg: "Task Manager MERN App API" }));
+// app.get("/", (req, res) => res.json({ msg: "Task Manager MERN App API" }));
 
 // Defining routes for the app components
 // First parameter is the text for the route, second parameter is specifying which file will be required
 app.use("/api/users", require("./routes/users"));
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Production server:
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
